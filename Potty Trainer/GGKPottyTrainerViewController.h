34a3bfb13c4ffd71a6d3e7515feee2f6f32b1ae2
Geoffrey Hom <geoffhom@gmail.com>
2013-02-04 15:20:43 -0800

    Initial Commit

---
+++ Potty Trainer/GGKPottyTrainerViewController.h
@@ -0,0 +1,13 @@
+//
+//  GGKPottyTrainerViewController.h
+//  Potty Trainer
+//
+//  Created by Geoff Hom on 2/4/13.
+//  Copyright (c) 2013 Geoff Hom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GGKPottyTrainerViewController : UIViewController
+
+@end
---
+++ Potty Trainer/GGKPottyTrainerViewController.m
@@ -0,0 +1,29 @@
+//
+//  GGKPottyTrainerViewController.m
+//  Potty Trainer
+//
+//  Created by Geoff Hom on 2/4/13.
+//  Copyright (c) 2013 Geoff Hom. All rights reserved.
+//
+
+#import "GGKPottyTrainerViewController.h"
+
+@interface GGKPottyTrainerViewController ()
+
+@end
+
+@implementation GGKPottyTrainerViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
